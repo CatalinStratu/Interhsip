af7a1dd39033b811952950de313de48132b53c65
Catalin Stratu <catalinstratu45@gmail.com>
2021-08-10 17:49:50 +0300

    Microservice structure

---
+++ Companies-Jobs-Candidates-microservice/src/main/java/com/example/companiesjobscandidatesmicroservice/features/job/type/Type.java
@@ -0,0 +1,12 @@
+package com.example.companiesjobscandidatesmicroservice.features.job.type;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table(name = "jobType")
+public class Type {
+}
