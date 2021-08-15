273d932cb2bb8edaa453046abf5e6fbb097b4bff
Catalin Stratu <catalinstratu45@gmail.com>
2021-08-15 12:32:43 +0300

    New API gateway

--- api-gateway-new/src/test/java/com/apigatewaynew/ApiGatewayNewApplicationTests.java
+++ api-gateway-new/src/test/java/com/apigatewaynew/ApiGatewayNewApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gateway.apigateway;
+package com.apigatewaynew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class ApiGatewayNewApplicationTests {
 
     @Test
     void contextLoads() {
